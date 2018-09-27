de2c70dfce816412767c13a496235b577d18a945
Ben Strutt <strutt@physics.ucla.edu>
2018-09-27 13:35:14 +0100

    Add run selection text widget

--- BeaconTau/Viewer.py
+++ BeaconTau/Viewer.py
@@ -3,7 +3,7 @@ from .RunAnalyzer import RunAnalyzer
 from matplotlib import pyplot as plt
 
 #from matplotlib.widgets import Slider, Button, RadioButtons
-from matplotlib.widgets import Button
+from matplotlib.widgets import Button, TextBox
 
 from enum import Enum
 
@@ -11,21 +11,20 @@ class Domain(Enum):
     Time = 0
     Freq = 1
 
+
 class Viewer():
     """
     A class to manage the viewing of events.
     And that's it.
     """
-    
+
     def __init__(self, run = None, data_dir = None):
         self.dd = DataDirectory(data_dir)
-        self.run = run
-        if self.run is None:
-            self.run = self.dd.runs[0]
-        print(self.run)
-        self.run_analyzer = self.dd.run(self.run)
-        self.entry = 0
-        self.event_analyzer = self.run_analyzer.get_entry(self.entry)
+
+        self.run = None
+        if run is None:
+            run = self.dd.runs[0]
+
         self.n_rows = 2
 
         self.domain = Domain.Time
@@ -41,11 +40,12 @@ class Viewer():
         self.stop_button = None
         self.timer = None
         self.playing = False
-
+ 
+        self.run_selection = None
         plt.ion()
         plt.show()
 
-        self.update()
+        self.set_run(run)
 
     def __del__(self):
         if self.timer is not None and self.playing is True:
@@ -59,7 +59,7 @@ class Viewer():
     def backward(self, event):
         self.entry = self.entry - 1
         self.event_analyzer = self.run_analyzer.get_entry(self.entry)
-        self.update()        
+        self.update()
 
     def start_play(self, event):
         self.playing = True
@@ -71,7 +71,19 @@ class Viewer():
     def stop_play(self, event):
         self.playing = False
         self.timer.stop()
-        
+
+    def set_run(self, run):
+        run = int(run)
+        if run is not self.run:
+            if run not in self.dd.runs:
+                raise ValueError('No run ' + str(run) + ' in ' + str(self.dd))
+
+            self.run = run
+            self.run_analyzer = self.dd.run(self.run)
+            self.entry = 0
+            self.event_analyzer = self.run_analyzer.get_entry(self.entry)
+            self.update()
+
     def __repr__(self):
         return '<BeaconTau.Viewer run ' + self.run + ' event ' + self.event_analyzer.header.event_number + '>'
 
@@ -92,7 +104,7 @@ class Viewer():
                 color = 'C' + str(chan) # matplotlib accepts strings beginning with a capital C for colors
                 self.axes.flat[chan].clear()
                 if self.domain is Domain.Time:
-                    self.axes.flat[chan].plot(self.event_analyzer.times(),  self.event_analyzer.channel(chan), color)
+                    self.axes.flat[chan].plot(self.event_analyzer.times(),  self.event_analyzer.channel(chan), color) 
                     self.axes.flat[chan].set_xlabel('Time (ns)')
                     self.axes.flat[chan].set_ylabel('Amplitude (mV)')
                 else:
@@ -103,7 +115,7 @@ class Viewer():
                     else:
                         self.axes.flat[chan].plot(self.event_analyzer.freqs(), self.event_analyzer.channel_psd(chan), color)
                         self.axes.flat[chan].set_ylabel('PSD (mV^{2} / MHz)')
-            plt.suptitle('Event ' + str(self.event_analyzer.event.event_number))
+            plt.suptitle('Run ' + str(self.run) + ' Event ' + str(self.event_analyzer.event.event_number))
 
         hover_color = 'white'
         # For these btton axes the list is [left, bottom, width, height]
@@ -116,7 +128,7 @@ class Viewer():
             stop_ax = plt.axes([0.9, 0.95, 0.1, 0.05])
             self.stop_button = Button(stop_ax, 'Stop', hovercolor='darkred', color='red')
             self.stop_button.on_clicked(self.stop_play)
-        
+
         if self.next_button is None:
             next_ax = plt.axes([0.8, 0.9, 0.1, 0.05])
             self.next_button = Button(next_ax, 'Next', hovercolor='blue', color='lightblue')
@@ -126,3 +138,9 @@ class Viewer():
             prev_ax = plt.axes([0.9, 0.9, 0.1, 0.05])
             self.prev_button = Button(prev_ax, 'Prev', hovercolor='yellow', color='lightyellow')
             self.prev_button.on_clicked(self.backward)
+
+        if self.run_selection is None:
+            run_ax = plt.axes([0.05, 0.95, 0.05, 0.03])
+            self.run_selection = TextBox(run_ax, 'Run', initial = str(self.run), color = 'white',  hovercolor = 'lightgrey')
+            self.run_selection.on_submit(self.set_run)
+
